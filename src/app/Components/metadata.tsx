7dbd6ca1e09fd655016bf069899c309d692ad1e6
unknown <apurvjha123@gmail.com>
2023-11-17 21:09:45 +0530

    cdn

--- src/app/Components/metadata.tsx
+++ src/app/Components/metadata.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next"
 export function constructMetadata({
     title = "Chit Chat - A chatbot Intregation using ChatGPT",
     description = "Transform your data into an interactive chatbot with dynamic API endpoints. Seamlessly integrate, engage users, and harness the power of conversational AI. You are one-stop destination for effortlessly bringing intelligent chatbots to your website without any coding hassles in just a few clicks.",
-    image = "/thumbnail.png",
+    image = "https://cdn.jsdelivr.net/gh/apurvjha123/chit-chat-nextjs/public/thumbnail.png",
     icons = "/favicon.ico",
     noIndex = false
   }: {
