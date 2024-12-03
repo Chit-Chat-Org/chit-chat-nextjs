7168e7584cb8730710dee12d5b637eb7a5c95512
unknown <apurvjha123@gmail.com>
2024-12-04 03:31:37 +0530

    removing dev mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,33 +2,40 @@ import { Footer, Navbar } from "@/components";
 import { SITE_CONFIG } from "@/config";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 const font = Inter({ subsets: ["latin"] });
 
 export const metadata = SITE_CONFIG;
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
-                    font.className
-                )}
-            >
-                <ClerkProvider appearance={{ baseTheme: dark }}>
-                    {children}
-                <Toaster />
-                </ClerkProvider>
-            </body>
-        </html>
-    );
-};
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
+          font.className
+        )}
+      >
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+            layout: {
+              unsafe_disableDevelopmentModeWarnings: true,
+            },
+          }}
+        >
+          {children}
+          <Toaster />
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
